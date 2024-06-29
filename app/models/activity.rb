0e8ce898d59a26950911b100b14ba1b799208dda
aureliendieffenbach <aurelien.dieffenbach@gmail.com>
2024-06-29 14:41:46 +0200

    config models activity and booking

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,4 +5,13 @@ class Activity < ApplicationRecord
   belongs_to :user
 
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,7 @@ class Booking < ApplicationRecord
 
   belongs_to :activities
   belongs_to :users
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
