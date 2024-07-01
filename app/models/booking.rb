c74934d1093f69fba9c84e9df4436a917ea5d4cb
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-07-01 19:53:02 +0200

    Création index show et new booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,4 +6,12 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  def duration_in_days
+    (end_date.to_date - start_date.to_date).to_i + 1
+  end
+
+  def total_price
+    duration_in_days * activity.price
+  end
 end
