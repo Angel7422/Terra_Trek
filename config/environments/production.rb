e8676ecc7cc3d53b3ba1d05ac40cb061c781660a
ange.saavedra17@gmail.com <ange.saavedra17@gmail.com>
2024-07-10 21:00:06 +0200

    add redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
