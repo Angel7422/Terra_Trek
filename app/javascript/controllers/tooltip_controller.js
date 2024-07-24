1e37db9deff076733aeed348dc86a22a034f6286
ange.saavedra17@gmail.com <ange.saavedra17@gmail.com>
2024-07-24 21:13:55 +0200

    finish

---
+++ app/javascript/controllers/tooltip_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tooltip"
+export default class extends Controller {
+  connect() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  }
+}
